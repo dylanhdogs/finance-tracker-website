2bbabb30b6fb7275ba317f090d59191509d38559
dylanhdogs <62785827+dylanhdogs@users.noreply.github.com>
2026-06-09 01:21:01 -0400

    Restructure page narrative: rewrite HowSection as transformation story, remove AudienceSection, tighten order

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import HeroSection from "./components/sections/HeroSection";
 import ProblemSection from "./components/sections/ProblemSection";
 import SharedExpensesSection from "./components/sections/SharedExpensesSection";
 import SolutionSection from "./components/sections/SolutionSection";
+import HowSection from "./components/sections/HowSection";
 import LoveSection from "./components/sections/LoveSection";
 import ScreenshotsSection from "./components/sections/ScreenshotsSection";
-import AudienceSection from "./components/sections/AudienceSection";
 import CompareSection from "./components/sections/CompareSection";
 import TestimonialsSection from "./components/sections/TestimonialsSection";
 import CtaSection from "./components/sections/CtaSection";
@@ -23,9 +23,9 @@ export default function App() {
         <ProblemSection />
         <SharedExpensesSection />
         <SolutionSection />
+        <HowSection />
         <LoveSection />
         <ScreenshotsSection />
-        <AudienceSection />
         <CompareSection />
         <TestimonialsSection />
         <CtaSection />
